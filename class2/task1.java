2514b9c85058658a05f9565f4a0a74398f4c4cc7
Yeswanth <kolayeswanth2005@gmail.com>
2026-01-27 10:02:05 +0530

    Add Task1 class to calculate exam marks average using arrays

---
+++ class2/task1.java
@@ -0,0 +1,18 @@
+// Exam marks average using arrays
+
+public class task1{
+    public static void main(String[] args){
+        int[] marks = {80,80,76,91,67,69};
+        int avg = 0;
+
+        for(int i = 0; i < marks.length-1; i++){
+            avg+= marks[i];
+        }
+        //total marks
+        System.out.println("Total marks are:" +avg);
+        //average marks
+        System.out.println("Average is:"+(avg/marks.length));
+
+    }
+
+}
