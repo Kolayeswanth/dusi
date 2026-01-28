b5b617f2c7a59f95e4553c9163d43fe3d6fb911e
Yeswanth <kolayeswanth2005@gmail.com>
2026-01-28 09:36:05 +0530

    Add additional patterns to Task7 class using nested loops

--- class2/Task7.java
+++ class2/Task7.java
@@ -44,6 +44,41 @@ public class Task7 {
             
             System.out.println();
         }
+        //      1
+        //     1 2
+        //    1 2 3
+        //   1 2 3 4
+        //  1 2 3 4 5
+        for(int i =1; i<=5;i++){
+            for(int j =i;j<=5;j++){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=i;j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+
+        //     1
+        //    212
+        //   32123
+        //  4321234
+        // 543212345
+        for(int i =1; i<=5;i++){
+            for(int j =i;j<=5;j++){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=i;j++){
+                System.out.print(j);
+            }
+            for(int j = 1;j<=i;j++){
+
+                if(j==1) continue;
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
 
     }
     
