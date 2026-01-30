287ae5ccb75d3b0291207aea76083642b3d0190b
Yeswanth <kolayeswanth2005@gmail.com>
2026-01-30 10:07:09 +0530

    Add lecture2 class to demonstrate method overriding in Java

--- class5/lecture1.java
+++ class5/lecture1.java
@@ -1,10 +1,38 @@
 // Types of inheritence is discussed in this class
 // different types of inheritence java is accepting is discussed in this class
-// java doesn't support mulitple inheritence
 // java supports single level multi level, hybrid and hierarchical inheritence
 // java doesn't support multiple inheritence to avoid ambiguity problem when a class inherits from two classes having same methods then when we call the constructer from child class it will get into ambiguity to call which parent class constructor so to avoid this problem java doesn't support multiple inheritence
 
+// method overloading and overriding is also discussed in this class
+// below code is related to overloading and overriding concept
+
+
+class A {
+    void show(){
+        System.out.println("Class A show method called");
+    }
+}
+
+class B extends A {
+    void show(){
+        System.out.println("Class B show method called");
+    }
+    void display(){
+        System.out.println("Class B display method called");
+    }
+}
 
 public class lecture1 {
+    public static void main(String[] args){
+        // creating object of class B
+        B objB = new B();
+        objB.show(); // this will call the show method of class B due to method overriding
+        objB.display(); // this will call the display method of class B
+
+        // creating object of class A
+        A objA = new A();
+        objA.show(); // this will call the show method of class A
     
+}
+
 }
---
+++ class5/lecture2.java
@@ -0,0 +1,31 @@
+// method overriding example
+
+class payment{
+    void pay(){
+        System.out.println("Payment done using cash");
+    }
+}
+class upi extends payment{
+    void pay(){
+        System.out.println("Payment done using UPI");
+    }
+}
+
+class creditcard extends payment{
+    void pay(){
+        System.out.println("Payment done using credit card");
+    }
+}
+
+public class lecture2 {
+    public static void main(String[] args){
+        payment p1 = new payment();
+        p1.pay(); // this will call pay method of payment class
+
+        payment p2 = new upi();
+        p2.pay(); // this will call pay method of upi class due to method overriding
+
+        payment p3 = new creditcard();
+        p3.pay(); // this will call pay method of creditcard class due to method overriding
+    }
+}
