7de182d2a34b6acc1d6a123241e6e1af636aef08
Yeswanth <kolayeswanth2005@gmail.com>
2026-01-30 09:43:17 +0530

    Add lecture1 class with comments explaining inheritance concepts in Java

---
+++ class5/lecture1.java
@@ -0,0 +1,10 @@
+// Types of inheritence is discussed in this class
+// different types of inheritence java is accepting is discussed in this class
+// java doesn't support mulitple inheritence
+// java supports single level multi level, hybrid and hierarchical inheritence
+// java doesn't support multiple inheritence to avoid ambiguity problem when a class inherits from two classes having same methods then when we call the constructer from child class it will get into ambiguity to call which parent class constructor so to avoid this problem java doesn't support multiple inheritence
+
+
+public class lecture1 {
+    
+}
