276aa80da26c127c67dafdcac3f425d377ad104b
Yeswanth <kolayeswanth2005@gmail.com>
2026-02-02 12:41:25 +0530

    Add serialization support to lecture2 class; implement Serializable interface and mark name field as transient

--- class6/lecture2.java
+++ class6/lecture2.java
@@ -1,8 +1,15 @@
 // continution part is there in lecture3.java
+// serializable interface is implemented to make object serializable.
+// transient keyword is used to skip the field during serialization.
+// serializable interface is a marker interface (it has no methods to implement).
+// when an object is serialized, all fields marked as transient are not included in the serialized representation.
+// this is useful for sensitive information or fields that can be derived and do not need to be stored.
 
-public class lecture2 {
+import java.io.Serializable;
+
+public class lecture2 implements Serializable {
     private String id;
-    private String name;
+    transient private String name;
 
     public lecture2(String id, String name) {
         this.id = id;
