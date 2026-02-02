6bf00b5a38590dc43a734eb437f79f9c3c1833f7
Yeswanth <kolayeswanth2005@gmail.com>
2026-02-02 13:03:02 +0530

    Add lecture5 class to demonstrate exception handling in Java; implement try-catch blocks for NullPointerException and ArithmeticException

---
+++ class6/lecture5.java
@@ -0,0 +1,34 @@
+// Exceptions Handling in Java
+// try{
+//      code part which can raise excpetion
+//  }
+//  == code between try and catch is called protected code ==
+//  == code won't execute if written between the try and catch block and doesn't compile ==
+// catch(Exception_name variable_name){
+//      code to handle the exception
+//  }
+
+
+public class lecture5 {
+    public static void main(String[] args){
+        String str = null;
+        try{
+            System.out.println(str.length());
+        }
+        catch(NullPointerException e){
+            System.out.println("Exception caught: "+e);
+        }
+
+        int a = 10;
+        int b = 0;
+        try{
+            int c = a/b;
+            System.out.println("Result: "+c);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Exception caught: "+e);
+        }
+        System.out.println("Program continues after exception handling.");
+    }
+    
+}
