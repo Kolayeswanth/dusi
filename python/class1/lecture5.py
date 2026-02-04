76bb57a100943c81b62296315847e4ef41fd6479
Yeswanth <kolayeswanth2005@gmail.com>
2026-02-04 10:24:36 +0530

    Add dictionary demonstration in lecture5 class in Python

---
+++ python/class1/lecture5.py
@@ -0,0 +1,13 @@
+#dictionary demonstration
+
+student = {'name': 'Alice', 'age': 21, 'courses': ['Math', 'CompSci']}
+print(type(student))  # Output: <class 'dict'>
+print(student['name'])  # Output: Alice
+print(student.get('age'))  # Output: 21
+student['age'] = 22
+print(student['age'])  # Output: 22
+student['grade'] = 'A'
+print(student)  # Output: {'name': 'Alice', 'age': 22, 'courses': ['Math', 'CompSci'], 'grade': 'A'}
+del student['grade']
+print(student)  # Output: {'name': 'Alice', 'age': 22, 'courses': ['Math', 'CompSci']}
+print(len(student))  # Output: 3
