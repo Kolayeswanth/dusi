30560ac6fe25c4b5f976011959a91d7468375fbe
Yeswanth <kolayeswanth2005@gmail.com>
2026-02-04 10:19:13 +0530

    Add range demonstration to lecture4 class in Python

--- python/class1/lecture4.py
+++ python/class1/lecture4.py
@@ -6,3 +6,9 @@ print(colors[0])  # Output: red
 print(colors[-1])  # Output: blue
 # colors[1] = 'yellow'  # This will raise a TypeError
 print(len(colors))  # Output: 3
+
+#range
+#range(start, stop, step)
+number = range(1, 10, 2)
+print(type(number))  # Output: <class 'range'>
+print(list(number))  # Output: [1, 3, 5, 7, 9]
