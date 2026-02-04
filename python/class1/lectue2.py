aee3ab788be0c258b467d2277f8517257f256ec3
Yeswanth <kolayeswanth2005@gmail.com>
2026-02-04 10:07:32 +0530

    Add lecture1 and lecture2 classes to demonstrate basic data type conversions and escape sequences in Python

---
+++ python/class1/lectue2.py
@@ -0,0 +1,12 @@
+# escape sequences
+
+print("hello \n world") #/n is used for new line
+print("hello \t world") # /t is used for tab space
+print("hello \\ world") # \\ is used for backslash
+print("hello \' world") # /' is used for single quote in string
+print("hello \" world") # /" is used for double quote in string
+print("hello \b world") # /b is used for backspace
+print("hello \r world") # /r is used for carriage return
+print("hello \f world") # /f is used for form feed
+print("hello \v world") # /v is used for vertical tab
+print("hello \a world") # /a is used for alert (bell)
