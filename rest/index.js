a00c1a27d7e4852091e4965cd5282e2aef131937
yeswanth.kola <kolayeswanth2005@gmail.com>
2026-02-11 10:55:41 +0530

    rest

---
+++ rest/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+});
+app.get('/name',(req,res)=>{
+    res.send('My name is Dusi');
+});
+app.post('/name',(req,res)=>{
+    name = req.body;
+    res.send(`My name is ${name}`);
+});
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
