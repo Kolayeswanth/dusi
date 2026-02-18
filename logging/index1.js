1234c5b009f3c3a45858cfbf716b5b1369dba7cc
yeswanth.kola <kolayeswanth2005@gmail.com>
2026-02-18 12:51:04 +0530

    feat(logging): add logging messages to app.log and create index1.js for logging functionality

---
+++ logging/index1.js
@@ -0,0 +1,6 @@
+const logger = require('./logger');
+
+logger.info('This is an info message');
+logger.warn('This is a warning message');
+logger.error('This is an error message');
+logger.debug('This is a debug message');
