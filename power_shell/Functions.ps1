68c2a7e979ac87940bb96b462e3ddcfbb2c8d919
yeswanth.kola <kolayeswanth2005@gmail.com>
2026-02-18 09:53:01 +0530

    feat: add Functions.ps1 with basic PowerShell functions for greeting and addition

---
+++ power_shell/Functions.ps1
@@ -0,0 +1,29 @@
+# functions in Powershell
+
+# 1.Basic Function
+
+function SayHello {
+    Write-Output "Hello From Powershell!"
+    
+}
+
+SayHello
+
+# 2.Function with Parameters
+function Greet {
+    param($name,$age)
+    Write-Output "Hello, $name! You are $age years old."
+}   
+
+Greet -name "Irfan" -age 25
+
+# 3.TypeCasting in Functions
+function add{
+    param(
+        [int]$num1,
+        [int]$num2
+    )
+    return $num1 + $num2
+}
+$result = add -num1 5 -num2 10
+Write-Output "The sum is: $result"
